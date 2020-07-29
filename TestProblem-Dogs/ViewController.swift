c9b4875e69a5c52c5b862803569ede29ad3a7ce8
Andrey Bogdanov <andbog@live.ru>
2020-07-29 18:23:04 +0300

    Initial Commit

---
+++ TestProblem-Dogs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProblem-Dogs
+//
+//  Created by Andrey Bogdanov on 29.07.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
